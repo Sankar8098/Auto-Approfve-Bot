f20e0860e8c53c47a10fe0b691edf44c3c8886d6
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-07-30 17:08:00 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,12 +3,12 @@ from os import path, getenv
 SUDO = "5171347305"
 
 class Config:
-    API_ID = int(getenv("API_ID", "14055090"))
-    API_HASH = getenv("API_HASH", "a46f7b439d0afa45b7a69fc450f754e9")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5616982320:AAFk0OhEkbFuUMrSSs6dfLdBYhxUI0c9Pt4")
-    FSUB = getenv("FSUB", "EmoBotDevolopers")
-    CHID = int(getenv("CHID", "-1001551504918"))
+    API_ID = int(getenv("API_ID", "23990433"))
+    API_HASH = getenv("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6324395748:AAGU-qGihOAcRgrxfdccuyySk7ST6hko4MU")
+    FSUB = getenv("FSUB", "")
+    CHID = int(getenv("CHID", "-1001870015374"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
